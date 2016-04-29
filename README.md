49ed66c8c18a7811237e361f25294325c0fcc08d
lostsnow <lostsnow@gmail.com>
2016-04-29 23:31:17 +0800

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Pinboard+
+=========
+
+A better firefox add-on for [Pinboard.in](http://pinboard.in).
+
+Thanks
+------
+
+This Firefox add-on was ported from the Chrome extension at [Pinboard-Plus](https://github.com/clvrobj/Pinboard-Plus)
---
+++ js/options.js
@@ -0,0 +1,18 @@
+$(function() {
+    // localStorage only stores string values
+    $('#no-ping').attr('checked', localStorage[nopingKey] === 'true')
+    .click(function () {
+        var value = $(this).is(':checked');
+        localStorage[nopingKey] = value;
+    });
+    $('#all-private').attr('checked', localStorage[allprivateKey] === 'true')
+    .click(function () {
+        var value = $(this).is(':checked');
+        localStorage[allprivateKey] = value;
+    });
+    $('#use-blockquote').attr('checked', isBlockquote())
+    .click(function () {
+        var value = $(this).is(':checked');
+        localStorage[noblockquoteKey] = !value;
+    });
+});
